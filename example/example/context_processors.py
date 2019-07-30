6630920a07c9f21bc5d40d02b9ac40cd6001a92a
Mohammad Abouchama <mohammadabouchama@gmail.com>
2019-07-30 12:58:42 +0400

    Remove example directory and its code base

--- example/example/context_processors.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-
-
-def solid_i18n(request):
-    example_vars = {
-        'SOLID_I18N_USE_REDIRECTS': settings.SOLID_I18N_USE_REDIRECTS,
-    }
-    return {"example_vars": example_vars}
